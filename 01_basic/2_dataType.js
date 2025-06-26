d127f341489d61052fec1995053c88a0ac725fde
Aditay Sharma <137498537+AditayCodes@users.noreply.github.com>
2025-06-26 19:32:16 +0000

    datatype done

---
+++ 01_basic/2_dataType.js
@@ -0,0 +1,23 @@
+"use strict";    // treat all js code as newer version
+
+// alert(3 + 3 ) ----------- we are using nodejs not browser
+
+console.log(3 + 3)
+
+console.log("Adi")
+
+/*
+number =>2 to power 53
+bigint
+string => ""
+boolen => t/f
+null => standalone value
+undefined =>
+symbol => unique
+
+object ---
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
