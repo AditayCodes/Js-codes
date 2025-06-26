d91d8bacc7630aaad48fea5da75d6b0918b2c9a0
Aditay Sharma <137498537+AditayCodes@users.noreply.github.com>
2025-06-26 18:57:44 +0000

    variable config done

---
+++ 01_basic/variables.js
@@ -0,0 +1,16 @@
+const roll = 12500
+let unEmail="adicode@google.com"
+var uPass="12365"
+uCity="kolkata"
+let uState;
+
+// roll ---- not changed  as not allowed to change the  value of const 
+
+unEmail="adicode@gmail.com"
+uPass="12345"
+uCity="delhi"
+
+console.log(roll)
+
+console.table([unEmail,uPass,uCity,uState])
+
