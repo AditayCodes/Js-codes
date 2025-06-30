4f5b5322716ecbc91e2b39c5a07203c5d13fef5b
Aditay Sharma <137498537+AditayCodes@users.noreply.github.com>
2025-06-30 13:36:57 +0000

    Conversion commit

--- 01_basic/2_dataType.js
+++ 01_basic/2_dataType.js
@@ -1,5 +1,6 @@
 "use strict";    // treat all js code as newer version
 
+//alert(3 + 3 ) ReferenceError: alert is not defined
 // alert(3 + 3 ) ----------- we are using nodejs not browser
 
 console.log(3 + 3)
---
+++ 01_basic/3_converssionOperator.js
@@ -0,0 +1,36 @@
+// --- num + albha
+
+let score = "33ab"
+
+console.log(typeof(score));
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// --- null 
+
+let sc = null
+
+console.log(typeof(sc));
+console.log(typeof(sc));
+
+
+let val = Number(sc)
+console.log(typeof val);
+console.log(val);
+
+//----- undefined
+
+let se = undefined
+
+console.log(typeof(se));
+console.log(typeof(se));
+
+
+let valu = String(se)
+console.log(typeof valu);
+console.log(valu);
