5dc5f97ec0721fa21df755428256cb6d5fadb44f
Aditay Sharma <137498537+AditayCodes@users.noreply.github.com>
2025-07-04 18:29:05 +0000

    while and do while loop commited

---
+++ 06_loops/while.js
@@ -0,0 +1,29 @@
+//while loop 
+
+let i=0
+while (i<=10) {
+    console.log(`the value is ${i}`);
+    i=i+2
+}
+console.log("***********************************************************");
+
+
+
+//do-while loop
+
+let j = 1
+do {
+    console.log(`change value ${j}`);
+    j++
+} while (j <= 10);
+
+
+console.log("////////////////////////////////////");
+
+
+//exceptional case in do while
+let k = 18
+do {
+    console.log(`change value ${k}`);
+    k++
+} while (k <= 10);
